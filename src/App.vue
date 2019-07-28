b5a69c37ea60710a6857944cd3e56e08cc5581cd
Kit Jonathan Te <kjonathante@gmail.com>
2019-07-27 23:51:06 -0700

    Used the v-for, v-bind and v-model directives.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
     <h4 class="bg-primary text-white text-center p-2">{{name}}'s' To Do List</h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+          {{t.done}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +22,13 @@ export default {
   name: 'app',
   data() {
     return {
-      name: 'Kit'
+      name: 'Kit',
+      tasks: [
+        { action: 'Buy Flowers', done: false },
+        { action: 'Get Shoes', done: false },
+        { action: 'Collect Tickets', done: true },
+        { action: 'Call Joe', done: false }
+      ]
     }
   }
 }
