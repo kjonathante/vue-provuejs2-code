7b20917795e532e95d5c8f11b12ce4eb961c9219
Kit Jonathan Te <kjonathante@gmail.com>
2019-07-29 22:36:28 -0700

    Added computed property which acts like data. Added a method to push an element into an array.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-2">{{name}}'s' To Do List</h4>
+    <h4 class="bg-primary text-white text-center p-2">{{name}}'s To Do List</h4>
     <div class="container-fluid p-4">
       <div class="row">
         <div class="col font-weight-bold">Task</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
         <div class="col">{{t.action}}</div>
-        <div class="col-2">
+        <div class="col-2 text-center">
           <input type="checkbox" v-model="t.done" class="form-check-input" />
-          {{t.done}}
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
         </div>
       </div>
     </div>
@@ -22,16 +35,27 @@ export default {
   name: 'app',
   data() {
     return {
-      name: 'Kit',
+      name: 'Adam',
       tasks: [
         { action: 'Buy Flowers', done: false },
         { action: 'Get Shoes', done: false },
         { action: 'Collect Tickets', done: true },
         { action: 'Call Joe', done: false }
-      ]
+      ],
+      hideCompleted: true,
+      newItemText: ''
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({ action: this.newItemText, done: false })
+      this.newItemText = ''
     }
   }
 }
 </script>
-
-
